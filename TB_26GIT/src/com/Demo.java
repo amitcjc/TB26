eded43c3596f9299c904dc1175117bd74878feda
amitcjc <amitcjcpune@gmail.com>
2024-07-08 09:08:53 +0530

    by update

--- TB_26GIT/src/com/Demo.java
+++ TB_26GIT/src/com/Demo.java
@@ -2,7 +2,15 @@ package com;
 
 public class Demo 
 {
+	public void m1()
+	{
+		System.out.println("This is M1===");
+	}
+	
+	
+	
 public static void main(String[] args) {
+
 	System.out.println("This is Main Method");
 }
 }
